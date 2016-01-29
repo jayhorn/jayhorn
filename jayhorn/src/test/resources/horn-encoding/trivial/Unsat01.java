141e153c7b076c8ff5a27b34f42ecc565d6acdeb
Temesghen Kahsai <lememta@gmail.com>
2016-01-29 10:44:42 -0800

    updated trivial

--- jayhorn/src/test/resources/horn-encoding/trivial/Unsat01.java
+++ jayhorn/src/test/resources/horn-encoding/trivial/Unsat01.java
@@ -1,16 +1,14 @@
+import java.util.Random;
 
 class Unsat01
 {
-    private static int f(int n) {
-        if (n > 100)
-            return n - 10;
-        else
-            return f(f(n + 11));
-    }
+	  public static void main(String[] arg) {
+	    int i = 0;
+	    boolean b = new Random().nextBoolean();
 
-    public static void main(String[] args) {
-    	int x = args.length;
-        int y = f(x);
-        assert(x > 101 && y < 90); // does not hold
-    }
+	    while(true) {
+	      i++;
+	      assert(b);
+	    }
+	  }
 }
