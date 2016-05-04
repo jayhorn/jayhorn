81c39bd6371973f9af2b8badc049010b8fffdb85
Rody Kersten <rody.kersten@sv.cmu.edu>
2016-05-04 10:44:57 -0700

    Made 3 examples from Teme's trivial one and fixed oracles.

--- jayhorn/src/test/resources/horn-encoding/trivial/Sat02.java
+++ jayhorn/src/test/resources/horn-encoding/trivial/Sat02.java
@@ -1,8 +1,8 @@
-public class Sat01 {
+public class Sat02 {
 	public static void main(final java.lang.String[] args) {
 		int N = 4;
 		char[] input = new char[N];
 		int x=input.length;
-		assert (N == 4);
+		assert (x == 4);
 	}
 }
