14c048f4f4049316db41d04dd439e0993b3f6b79
schaef <martinschaef@gmail.com>
2016-09-05 18:37:47 -0700

    one more try with travis and removed checkstyle since no one is using it anyway

---
+++ deply_results.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -n "$GITHUB_API_KEY"] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+  cd "$TRAVIS_BUILD_DIR"
+  git checkout -b gh-pages
+  mkdir $BRANCH
+  cp -r jayhorn/build/reports/tests $BRANCH/
+  git add $BRANCH
+  git -c user.name='martinschaef' -c user.email='martinschaef@gmail.com' commit -m "travis update to test results." --no-verify
+  git push -f -q https://martinschaef:$GITHUB_API_KEY@github.com/jayhorn/jayhorn gh-pages &2>/dev/null
+  cd "$TRAVIS_BUILD_DIR"
+fi
