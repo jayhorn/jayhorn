2b441cb3072f5909e979d0d44e9cfef05e20b4b9
schaef <martinschaef@gmail.com>
2016-09-06 13:30:05 -0700

    maybe the deployment works now ...

--- deploy_results.sh
+++ deploy_results.sh
@@ -2,14 +2,17 @@
 
 if [ -n "$GITHUB_API_KEY" ]; then 
   if [ -n "$TRAVIS_BRANCH" ]; then
-    git checkout origin/gh-pages
-    echo $TRAVIS_BRANCH
+    mkdir -p web
+    git clone https://github.com/jayhorn/jayhorn.git  --branch gh-pages --single-branch web/
+    cd web
     mkdir -p ./$TRAVIS_BRANCH
-    cp -r ./jayhorn/build/reports/tests ./$TRAVIS_BRANCH/
+    cp -r ./../jayhorn/build/reports/tests ./$TRAVIS_BRANCH/
     echo "$PWD"    
     git add ./$TRAVIS_BRANCH
     git -c user.name='martinschaef' -c user.email='martinschaef@gmail.com' commit -m "travis update to test results." --no-verify
     git push -f -q https://martinschaef:$GITHUB_API_KEY@github.com/jayhorn/jayhorn gh-pages &2>/dev/null
-    git checkout $TRAVIS_BRANCH    
+    cd ..    
   fi
 fi
+
+
