dd56845df24e60bf63a5539d7e61770750f4e3b4
schaef <martinschaef@gmail.com>
2016-09-06 10:57:02 -0700

    intermediate commit

--- deploy_results.sh
+++ deploy_results.sh
@@ -2,17 +2,14 @@
 
 if [ -n "$GITHUB_API_KEY" ]; then 
   if [ -n "$TRAVIS_BRANCH" ]; then
-    echo "$PWD"
-    cd "$TRAVIS_BUILD_DIR"
-    echo "$PWD"
-    git checkout -b gh-pages origin/gh-pages
     echo $TRAVIS_BRANCH
     mkdir ./$TRAVIS_BRANCH
     cp -r ./jayhorn/build/reports/tests ./$TRAVIS_BRANCH/
-    git add ./$TRAVIS_BRANCH
-    git -c user.name='martinschaef' -c user.email='martinschaef@gmail.com' commit -m "travis update to test results." --no-verify
-    git push -f -q https://martinschaef:$GITHUB_API_KEY@github.com/jayhorn/jayhorn gh-pages &2>/dev/null
-    git checkout $TRAVIS_BRANCH
-    cd "$TRAVIS_BUILD_DIR"
+    echo "$PWD"
+    git checkout -b gh-pages origin/gh-pages
+    #git add ./$TRAVIS_BRANCH
+    #git -c user.name='martinschaef' -c user.email='martinschaef@gmail.com' commit -m "travis update to test results." --no-verify
+    #git push -f -q https://martinschaef:$GITHUB_API_KEY@github.com/jayhorn/jayhorn gh-pages &2>/dev/null
+    #git checkout $TRAVIS_BRANCH    
   fi
 fi
