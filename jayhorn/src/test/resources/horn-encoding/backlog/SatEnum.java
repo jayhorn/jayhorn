14a81cbdf2412194e754423c0254b1bf4658f504
Rody Kersten <rody.kersten@sv.cmu.edu>
2016-09-23 14:36:12 -0700

    simplify SatEnum

--- jayhorn/src/test/resources/horn-encoding/backlog/SatEnum.java
+++ jayhorn/src/test/resources/horn-encoding/backlog/SatEnum.java
@@ -1,12 +1,8 @@
 public class SatEnum {
 
 	public enum WaterLevelEnum {low, normal, high}
-	public WaterLevelEnum waterLevel = WaterLevelEnum.normal;
 
 	public static void main (String args[]) {
-		SatEnum a = new SatEnum();
-		//a.waterLevel = WaterLevelEnum.low;
-		//assert a.waterLevel!=null;
 		assert WaterLevelEnum.low != null;
 	}
 }
