ca46b983d1cba473500b4ab580fcc930000a64c6
Teme <lememta@gmail.com>
2016-09-28 14:31:35 -0700

    changing prints

--- jayhorn/src/main/java/jayhorn/Main.java
+++ jayhorn/src/main/java/jayhorn/Main.java
@@ -66,7 +66,7 @@ public class Main {
       }
     
 	public static void main(String[] args) {
-		System.out.println("\t\t ---   JAYHORN : Static Analayzer for Java Programs ---- ");
+		Log.info("\t\t ---   JAYHORN : Static Analayzer for Java Programs ---- ");
 		
 		Options options = Options.v();
 		CmdLineParser parser = new CmdLineParser(options);
