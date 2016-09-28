5ad256a2d4441a31d004814960b2be3837a39b54
Philipp Ruemmer <ph_r@gmx.net>
2016-09-28 22:30:54 +0200

    typo

--- jayhorn/src/main/java/jayhorn/Main.java
+++ jayhorn/src/main/java/jayhorn/Main.java
@@ -66,7 +66,7 @@ public class Main {
       }
     
 	public static void main(String[] args) {
-		System.out.println("\t\t ---   JAYHORN : Static Analayzer for Java Programs ---- ");
+		System.out.println("\t\t ---   JAYHORN : Static Analyzer for Java Programs ---- ");
 		
 		Options options = Options.v();
 		CmdLineParser parser = new CmdLineParser(options);
