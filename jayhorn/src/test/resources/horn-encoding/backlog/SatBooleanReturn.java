f974a8de53124541abc77361ae9fb7119e894a22
Rody Kersten <rody.kersten@sv.cmu.edu>
2017-01-09 17:47:44 -0800

    test case for new issue

---
+++ jayhorn/src/test/resources/horn-encoding/backlog/SatBooleanReturn.java
@@ -0,0 +1,10 @@
+class SatBooleanReturn {
+  boolean test() {
+    return true;
+  }
+
+  public static void main(String args[]) {
+    SatBooleanReturn x = new SatBooleanReturn();
+    x.test();
+  }
+}
