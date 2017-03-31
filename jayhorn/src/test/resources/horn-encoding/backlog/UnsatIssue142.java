c9080673f20c145abaa666cd49729c315649c360
Rody Kersten <rody.kersten@sv.cmu.edu>
2017-03-31 13:03:44 -0700

    Test for Issue 142

---
+++ jayhorn/src/test/resources/horn-encoding/backlog/UnsatIssue142.java
@@ -0,0 +1,23 @@
+public class UnsatIssue142 {
+
+    public static void sort(Object[] a) {
+        Object[] aux = new Object[a.length];
+        for (int i = 0; i < a.length; i++)
+            aux[i] = a[i];
+        assert false;
+    }
+
+    public static void main(String[] args) {
+	java.util.Random r = new java.util.Random();
+        final int N = r.nextInt(100);
+        if (N < 1) return;
+
+        Integer data[] = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            data[i] = r.nextInt(); 
+        }
+
+        sort(data);
+    }
+
+}
