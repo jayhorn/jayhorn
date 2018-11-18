1398c2b53db801acd0f70d7e613e01e75c117e3d
Philipp Ruemmer <ph_r@gmx.net>
2018-11-18 22:55:31 +0100

    assume that fields of java.lang.System are always initialised

---
+++ jayhorn/src/test/resources/horn-encoding/regression/SatJavaLangSystem.java
@@ -0,0 +1,9 @@
+
+public class SatJavaLangSystem {
+    public static void main(String args[]) {
+        int x = 5;
+//        System.out.println("Test: " + x);
+        System.out.println("Test!");
+        assert x > 0;
+    }
+}
