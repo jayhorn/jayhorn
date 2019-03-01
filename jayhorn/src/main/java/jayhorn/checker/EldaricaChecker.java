2bf81f2d31efa08590d80649cc739c98ebb150c5
Philipp Ruemmer <ph_r@gmx.net>
2019-03-01 09:30:00 +0100

    use a unique file name for horn clause output

--- jayhorn/src/main/java/jayhorn/checker/EldaricaChecker.java
+++ jayhorn/src/main/java/jayhorn/checker/EldaricaChecker.java
@@ -41,6 +41,8 @@ public class EldaricaChecker extends Checker {
     private ProverFactory factory;
     private Prover prover;
 
+    private int hornOutputNum = 0;
+
     public EldaricaChecker(ProverFactory factory) {
         this.factory = factory;
     }
@@ -180,8 +182,9 @@ public class EldaricaChecker extends Checker {
 
             allClauses.add(entryClause);
 
-            Hornify.hornToSMTLIBFile(allClauses, 0, prover);
-            Hornify.hornToFile(allClauses, 0);
+            Hornify.hornToSMTLIBFile(allClauses, hornOutputNum, prover);
+            Hornify.hornToFile(allClauses, hornOutputNum);
+            hornOutputNum = hornOutputNum + 1;
 
             for (ProverHornClause clause : allClauses)
                 prover.addAssertion(clause);
